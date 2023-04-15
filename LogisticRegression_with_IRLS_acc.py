8370147b52865d466db01eea9d89e4235ed2fc6e
Agata Kaczmarek <56119853+kaczmareka@users.noreply.github.com>
2023-04-15 13:46:26 +0200

    done part 2 + added comments

--- LogisticRegression_with_IRLS_acc.py
+++ LogisticRegression_with_IRLS_acc.py
@@ -3,8 +3,15 @@ from numpy.linalg import inv
 from sklearn.metrics import accuracy_score
 
 class LogisticRegression_with_IRLS:
-    """Linear Regression with Iterative Reweighted Least Squares optimization"""
+    """Linear Regression with Iterative Reweighted Least Squares optimization."""
+
     def __init__(self, X_test=None, y_test=None, test_flag=False):
+        """Initializes the model
+
+        Args:
+            X_test (numpy.ndarray): The test dataset, needed if test_flag is True.
+            y_test (numpy.ndarray): The test labels, needed if test_flag is True.
+            test_flag (bool): If True, the model will be tested on the test dataset."""
                 
         # nr of instances
         self.n = None
@@ -25,14 +32,19 @@ class LogisticRegression_with_IRLS:
         self.y_test = y_test 
         
     def fit(self, X, y, interaction_ids = [], max_iter=100):
-        """ Fits the model using training data"""
+        """ Fits the model using training data.
+
+        Args:
+            X (numpy.ndarray): The training dataset.
+            y (numpy.ndarray): The training labels.
+            interaction_ids (list): A list of tuples, each tuple containing the ids of the variables to be multiplied.
+            max_iter (int): The maximum number of iterations of the IRLS algorithm."""
         
         # test if X matrix and y vector of the same length
         np.testing.assert_equal(np.shape(X)[0], len(y))
         
         # test if y has only classes 0 and 1
         np.testing.assert_equal(np.unique(y),[0,1])
-        
         
         # n - nr of instances
         self.n = np.shape(X)[0]
@@ -42,22 +54,40 @@ class LogisticRegression_with_IRLS:
         self.p = np.shape(X)[1]
         
         self.interaction_ids = interaction_ids
+
+        # for testing more parameters
         if self.test_flag:
             self.beta, self.X, self.acc_all, self.tol_all = self.IRLS_with_interactions(X, y, self.interaction_ids, max_iter=max_iter)
         else:
             self.beta, self.X = self.IRLS_with_interactions(X, y, self.interaction_ids, max_iter=max_iter)
 
     def add_interactions(self, X, interaction_ids):
+        """ Adds interactions to the dataset.
+        
+        Args:
+            X (numpy.ndarray): The dataset.
+            interaction_ids (list): A list of tuples, each tuple containing the ids of the variables to be multiplied."""
+
         for interaction in interaction_ids:
             new_col = X[:,interaction[0]]*X[:,interaction[1]]
             X = np.insert(X, X.shape[1], new_col, axis=1)
         return X
 
-    def IRLS_with_interactions(self, X, y, interaction_ids, max_iter=100, w_init = 1, d = 0.0001, tolerance = 0.001):
+    def IRLS_with_interactions(self, X, y, interaction_ids, max_iter=100, w_init = 0.5, d = 0.0001, tolerance = 0.001):
+        """ Fits the model using training data.
         
+        Args:
+            X (numpy.ndarray): The training dataset.
+            y (numpy.ndarray): The training labels.
+            interaction_ids (list): A list of tuples, each tuple containing the ids of the variables to be multiplied.
+            max_iter (int): The maximum number of iterations of the IRLS algorithm.
+            w_init (float): The initial value of the weights.
+            d (float): The value of the d parameter.
+            tolerance (float): Tolerance threshold for the algorithm to stop."""
+
         if self.test_flag:
-            acc_all = np.zeros(max_iter-1)
-            tol_all = np.zeros(max_iter-1)
+            acc_all = []
+            tol_all = []
 
         X = self.add_interactions(X, interaction_ids)
         n,p = X.shape
@@ -74,45 +104,66 @@ class LogisticRegression_with_IRLS:
             B_new = np.dot(inv( X.T.dot(W).dot(X)), 
                     ( X.T.dot(W).dot(y) ) )
             tol = sum( abs( B_new - B_old ) ) 
-            if self.test_flag and i>0:
-                acc_all[i-1] = accuracy_score(self.y_test, self.predict_test(self.X_test, B_new))
-                tol_all[i-1] = tol
-                return B_new, X, acc_all, tol_all
+            if self.test_flag:
+                acc_all.append(accuracy_score(self.y_test, self.predict_test(self.X_test, B_new)))
+                tol_all.append(tol)
             
             if tol < tolerance:
+                if self.test_flag:
+                    return B_new, X, acc_all, tol_all
                 return B_new, X
+            
+        if self.test_flag:
+            return B_new, X, acc_all, tol_all
         return B_new, X
     
     def predict_proba(self, Xtest):
-        """ Predicts posterior probabilities for class 1 for observations whose featurevalues are in rows of the matrix Xtest"""
+        """ Predicts posterior probabilities for class 1 for observations whose featurevalues are in rows of the matrix Xtest.
+        
+        Args:
+            Xtest (numpy.ndarray): The test dataset."""
+
         Xtest = self.add_interactions(Xtest, self.interaction_ids)
         pi = (np.exp(np.dot(Xtest, self.beta.T)))/(1+np.exp(np.dot(Xtest, self.beta.T)))
         
         return pi
     
     def predict(self, Xtest):
-        """ Assigns the predicted class (0 or 1) for observations whose feature values are in rows of the matrix Xtest"""
+        """ Assigns the predicted class (0 or 1) for observations whose feature values are in rows of the matrix Xtest.
+        
+        Args:
+            Xtest (numpy.ndarray): The test dataset."""
+        
         pi = self.predict_proba(Xtest)
         y_pred = np.multiply([pi>0.5], 1)[0];
         
         return y_pred
     
     def get_params(self):
-        """returns a list containing the estimated parameters beta """
+        """Returns a list containing the estimated parameters beta."""
     
         return self.beta
     
     def predict_proba_test(self, Xtest, beta):
-        """ Predicts posterior probabilities for class 1 for observations whose featurevalues are in rows of the matrix Xtest"""
+        """ Predicts posterior probabilities for class 1 for observations whose feature values are in rows of the matrix Xtest.
+        
+        Args:
+            Xtest (numpy.ndarray): The test dataset.
+            beta (numpy.ndarray): The estimated beta."""
+        
         Xtest = self.add_interactions(Xtest, self.interaction_ids)
         pi = (np.exp(np.dot(Xtest, beta.T)))/(1+np.exp(np.dot(Xtest, beta.T)))
         
         return pi
     
     def predict_test(self, Xtest, beta):
-        """ Assigns the predicted class (0 or 1) for observations whose feature values are in rows of the matrix Xtest"""
+        """ Assigns the predicted class (0 or 1) for observations whose feature values are in rows of the matrix Xtest.
+        
+        Args:
+            Xtest (numpy.ndarray): The test dataset.
+            beta (numpy.ndarray): The estimated beta."""
+
         pi = self.predict_proba_test(Xtest, beta)
         y_pred = np.multiply([pi>0.5], 1)[0];
         
         return y_pred
-    
---
+++ utils.py
@@ -0,0 +1,207 @@
+import numpy as np
+np.random.seed(123)
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.model_selection import train_test_split
+from LogisticRegression_with_IRLS_acc import LogisticRegression_with_IRLS
+import matplotlib.pyplot as plt
+from scipy.stats import bernoulli
+
+def sigmoid(z):
+    """Sigmoid function for logistic regression."""
+    
+    return 1.0 / (1.0 + np.exp(-z))
+
+def generate_dataset1(size, num_features=2):
+    """Generates a dataset with a given size and number of features.
+    
+    Args:
+        size (int): Number of observations.
+        num_features (int): Number of features. """
+    
+    np.random.seed(123)
+    first_dataset_X = np.random.normal(size=(size, num_features))
+    first_dataset_y = np.random.binomial(1, sigmoid(np.dot(np.column_stack((np.ones(size), first_dataset_X)), np.array([i for i in range (num_features+1)]))))
+    return first_dataset_X, first_dataset_y
+
+
+def generate_dataset2(size):
+    """Generates a dataset with a given size and number of features.
+
+    Args:
+        size (int): Number of observations."""
+
+    b = np.array([2.3, 4.5, 0.345])
+    second_dataset_X = np.zeros((size,3))
+    for i in range(size):
+        second_dataset_X[i][0]=np.random.normal(0,1,1)
+        second_dataset_X[i][1]=np.random.normal(3,2,1)
+        second_dataset_X[i][2]=np.random.normal(-5,3,1)
+    p=(1/(1+np.exp(-(np.dot(second_dataset_X, b.T)))))
+    second_dataset_y= bernoulli.rvs(p, size=size)
+    return second_dataset_X, second_dataset_y
+
+
+def generate_dataset3(m):
+    """Generates a dataset with a given size and number of features.
+    
+    Args:
+        m (int): Mean of the second class."""
+
+    dataset=np.zeros((1000, 3))
+    for i in range(500):
+        dataset[i,0]=np.random.normal(loc=0.0, scale=1.0)
+        dataset[i,1]=np.random.normal(loc=0.0, scale=1.0)
+        dataset[i,2]=0
+        dataset[i+500,0]=np.random.normal(loc=m, scale=0.5)
+        dataset[i+500,1]=np.random.normal(loc=m, scale=0.7)
+        dataset[i+500,2]=1
+    return dataset
+
+def generate_dataset4(variance):
+    """Generates a dataset with a given size and number of features.
+    
+    Args:
+        variance (float): Variance of the noise."""
+
+    dataset=np.zeros((1000, 3))
+    for i in range(500):
+        x_1=np.random.uniform(low=-1, high=1)
+        x_2=np.sqrt(1-x_1**2)
+        dataset[i,0]=x_1
+        if i%2==0:
+            dataset[i,1]=x_2*(-1)
+        else:
+            dataset[i,1]=x_2
+        dataset[i,2]=0
+        x_1=np.random.uniform(low=-2, high=2)
+        x_2=np.sqrt(4-x_1**2)
+        dataset[i+500,0]=x_1
+        if i%2==0:
+            dataset[i+500,1]=x_2*(-1)
+        else:
+            dataset[i+500,1]=x_2
+        dataset[i+500,2]=1
+    dataset[:, 0:2]=dataset[:, 0:2]+np.random.normal(loc=0.0, scale=variance, size=(1000, 2))
+    return dataset
+
+def generate_xor(size):
+    """Generates a XOR dataset with a given size.
+
+    Args:
+        size (int): Number of observations."""
+
+    xor_X = np.random.uniform(-1, 1, (size, 2))
+    xor_y = np.logical_xor(xor_X[:, 0] > 0, xor_X[:, 1] > 0).astype(int)
+    return xor_X, xor_y
+
+
+def run_experiments(X, y, iterations=10):
+    """Runs the experiments for the given dataset.
+
+    Args:
+        X (numpy.ndarray): The dataset.
+        y (numpy.ndarray): The labels.
+        iterations (int): Number of iterations."""
+
+    acc_all=np.zeros((iterations, 2))
+    prec_all=np.zeros((iterations, 2))
+    rec_all=np.zeros((iterations, 2))
+    f1_all=np.zeros((iterations, 2))
+    for i in range(iterations):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=i)
+        clf_without_interactions = LogisticRegression_with_IRLS()
+        clf_without_interactions.fit(X_train, y_train, max_iter=500)
+        y_pred_without_interactions = clf_without_interactions.predict(X_test)
+        acc_all[i][0]=accuracy_score(y_test, y_pred_without_interactions)
+        prec_all[i][0]=precision_score(y_test, y_pred_without_interactions)
+        rec_all[i][0]=recall_score(y_test, y_pred_without_interactions)
+        f1_all[i][0]=f1_score(y_test, y_pred_without_interactions)
+        clf_with_interactions = LogisticRegression_with_IRLS()
+        clf_with_interactions.fit(X_train, y_train, interaction_ids = [[0,1],[0,2],[1,2]], max_iter=500)
+        y_pred_with_interactions = clf_with_interactions.predict(X_test)
+        acc_all[i][1]=accuracy_score(y_test, y_pred_with_interactions)
+        prec_all[i][1]=precision_score(y_test, y_pred_with_interactions)
+        rec_all[i][1]=recall_score(y_test, y_pred_with_interactions)
+        f1_all[i][1]=f1_score(y_test, y_pred_with_interactions)
+    return acc_all, prec_all, rec_all, f1_all
+
+
+def make_boxplot(acc, prec, rec, f1, name, dataset_name):
+    """Makes a boxplot for the given metrics.
+
+    Args:
+        acc (numpy.ndarray): Accuracy.
+        prec (numpy.ndarray): Precision.
+        rec (numpy.ndarray): Recall.
+        f1 (numpy.ndarray): F1 score.
+        name (str): Name of the metric.
+        dataset_name (str): Name of the dataset."""
+    
+    fig, axs = plt.subplots(2, 2, figsize=(15, 10))
+    fig.suptitle(f'{name} for models for {dataset_name}', fontsize=20)
+    axs[0, 0].boxplot(acc)
+    axs[0, 0].set_title('Accuracy', fontsize=15)
+    axs[0, 0].set_xticklabels(['Without interactions', 'With interactions'], fontsize=12)
+    axs[0, 1].boxplot(prec)
+    axs[0, 1].set_title('Precision', fontsize=15)
+    axs[0, 1].set_xticklabels(['Without interactions', 'With interactions'], fontsize=12)
+    axs[1, 0].boxplot(rec)
+    axs[1, 0].set_title('Recall', fontsize=15)
+    axs[1, 0].set_xticklabels(['Without interactions', 'With interactions'], fontsize=12)
+    axs[1, 1].boxplot(f1)
+    axs[1, 1].set_title('F1-score', fontsize=15)
+    axs[1, 1].set_xticklabels(['Without interactions', 'With interactions'], fontsize=12)
+
+
+def check_performance(X, y, dataset_name, if_interactions=False, interaction_ids=None):
+    """Checks the performance of the model.
+
+    Args:
+        X (numpy.ndarray): The dataset.
+        y (numpy.ndarray): The labels.
+        dataset_name (str): Name of the dataset.
+        if_interactions (bool): Whether to use interactions or not.
+        interaction_ids (list): List of ids of the features to interact."""
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    clf = LogisticRegression_with_IRLS()
+    if if_interactions:
+        clf.fit(X_train, y_train, max_iter=500, interaction_ids=interaction_ids)
+    else:
+        clf.fit(X_train, y_train, max_iter=500)
+    y_pred = clf.predict(X_test)
+    acc= accuracy_score(y_test, y_pred)
+    print(f'Accuracy: {acc}')
+
+    #draw results
+
+    fig, ax = plt.subplots(1, 2, figsize=(15, 6))
+    ax[0].scatter(X_test[:,0], X_test[:,1], c=y_test, cmap='winter')
+    ax[0].set_title('Original dataset', fontsize=20)
+    ax[1].scatter(X_test[:,0], X_test[:,1], c=y_pred, cmap='winter')
+    ax[1].set_title('Predicted classes', fontsize=20)
+    fig.suptitle(dataset_name, fontsize=20)
+    plt.show()
+
+
+### nwm czy używamy to co niżej
+
+
+def draw_boundary(X, y, coef, iter_num):
+    """Draws the boundary of the model.
+
+    Args:
+        X (numpy.ndarray): The dataset.
+        y (numpy.ndarray): The labels.
+        coef (numpy.ndarray): Coefficients of the model.
+        iter_num (int): Number of iterations."""
+
+    fig, ax = plt.subplots(figsize=(10, 10))
+    ax.scatter(X[:, 0], X[:, 1], c=y, cmap='winter')
+    x1 = np.linspace(-2, 2, 100)
+    m=-(coef[0])/coef[1]
+    c= - (coef[2])/coef[1]
+    y = m*x1 + c
+    ax.plot(x1, y)
+    ax.set_title(f'Boundary after {iter_num} iterations', fontsize=20)
+    plt.show()
